c49e48a33af2fc2c1c79de7f66973aa6d97b1c35
Delta Student <himanshusingh140302003@gmail.com>
2025-08-25 17:47:01 +0530

    all changes fixed

--- models/listing.js
+++ models/listing.js
@@ -14,6 +14,12 @@ const listingSchema = new Schema({
     price: Number,
     location: String,
     country: String,
+    reviews: [
+        {
+            type:Schema.Types.ObjectId,
+            ref:"Review",
+        },
+    ],
 });
 const Listing = mongoose.model("Listing", listingSchema);
 //main file ka sub-part hai.same folder me other file ko export karate hai.
